7ff221948a86f73325abc92137843014e329ff35
Anuj Samuel <anuj.samuellakra@gmail.com>
2021-09-17 21:22:58 -0700

    Refining the test and initalize variable

--- spec/simpleCalculator_spec.rb
+++ spec/simpleCalculator_spec.rb
@@ -1,29 +1,33 @@
 require 'simpleCalculator'
 
 describe 'SimpleCalculator' do
+
+    context 'When testing the SimpleCalculator class' do 
+
+        before(:each) do
+            @calc = SimpleCalculator.new(10,2)
+        end
     
-    it 'adds two numbers' do
-        calc = SimpleCalculator.new
-        output = calc.add(5,4)
-        expect(output).to eq(9)
-    end
+        it 'adds two numbers' do       
+            output = @calc.add
+            expect(output).to eq(12)
+        end
 
-    it 'substract two numbers' do
-        calc = SimpleCalculator.new
-        output = calc.substract(5,4)
-        expect(output).to eq(1)
-    end
+        it 'substract two numbers' do
+            output = @calc.substract
+            expect(output).to eq(8)
+        end
 
 
-    it 'multiply two numbers' do
-        calc = SimpleCalculator.new
-        output = calc.multiply(5,4)
-        expect(output).to eq(20)
+        it 'multiply two numbers' do
+            output = @calc.multiply
+            expect(output).to eq(20)
+        end
+
+        it 'divide two numbers' do
+            output = @calc.divide
+            expect(output).to eq(5)
+        end
     end
 
-    it 'divide two numbers' do
-        calc = SimpleCalculator.new
-        output = calc.divide(10,2)
-        expect(output).to eq(5)
-    end
 end
